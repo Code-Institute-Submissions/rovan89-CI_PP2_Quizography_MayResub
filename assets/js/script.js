c6c1e8e6fa57ece96eb7eebd4ae7505ada0ccefb
rovan89 <devireland89@gmail.com>
2021-11-14 20:27:10 +0000

    Add function to recive quiz answer from an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("Linked pages")
+// This is an array of the questions and their images
+let quizAnswers = [["Answer One!!!", "Answer Two", "Answer Three", "Answer Four"],["Answer One!!!", "Answer Two", "Answer Three", "Answer Four"]]
+
+
+
+/**
+ * This function loads the image to the quiz
+ */
+function loadQuiz() {
+    //let quizBtns = document.getElementsByClassName("quiz_btns");
+    let answerOne = document.getElementById("answer_one")
+    answerOne.innerHTML = quizAnswers[0][1];
+}
+
+loadQuiz();
+console.log(quizBtns)
