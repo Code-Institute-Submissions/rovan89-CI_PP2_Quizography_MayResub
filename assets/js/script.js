38e03f162bb03919cca88985477cf384110260ad
rovan89 <devireland89@gmail.com>
2021-11-15 21:17:45 +0000

    Add load quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,33 @@
 // This is an array of the questions and their images
 let quizAnswers = [["Answer One!!!", "Answer Two", "Answer Three", "Answer Four"],["Answer One***", "Answer Two", "Answer Three", "Answer Four"]]
-
+let quizImages = ['<img src="assets/images/quiz-images/pexels-jf-valeriano-3609894.jpg" alt="Girl in a jacket" width="500" height="600">']
 let answerPosition = 0;
+let imagePosition = 0;
 
 
 // Questions 
+
+function loadQuizImage() {
+    let showQuizImage = document.getElementById("main_image_container");
+    showQuizImage.innerHTML = quizImages[0] ;   
+}
+loadQuizImage();
+function changeQuizImage() {
+    let arrayOfImages = quizImages.length
+    for (let i = 0; i < arrayOfImages; i++) {
+         imagePosition++;
+    }
+    
+}
+
+
+
 
 /**
  * This function loads the image to the quiz
  */
 function loadQuiz() {
-    let answerOne = document.getElementById("answer_one", "answer_two", "answer_three", "answer_four")
+    let answerOne = document.getElementById("answer_one")
     answerOne.innerHTML = quizAnswers[answerPosition][0];
     let answerTwo = document.getElementById("answer_two")
     answerTwo.innerHTML = quizAnswers[answerPosition][1];
@@ -29,7 +46,7 @@ loadQuiz();
 function nextQuestion() {
     let arrayOfAnswers = quizAnswers.length
     for (let i = 0; i < arrayOfAnswers; i++) {
-         answerPosition++
+         answerPosition++;
     }
 }
 
