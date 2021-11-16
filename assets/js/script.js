f410da2aaf8f3974dd94fc1856f7d2f7064b84ec
rovan89 <devireland89@gmail.com>
2021-11-16 15:30:47 +0000

    Add iteration to answer of answerBtns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 // This is an array of the questions and their images
-let quizAnswers = [["Answer One!!!", "Answer Two", "Answer Three", "Answer Four"],["Answer One***", "Answer Two", "Answer Three", "Answer Four"]];
+let quizAnswers = [
+    {
+        image: '<img src="assets/images/quiz-images/pexels-jf-valeriano-3609894.jpg" alt="Girl in a jacket">',
+        answers: [
+            {text: 'Bond', corrent: true},
+            {text: '300', correct: false}
+        ]
+    }
+    
+    ["Answer One!!!", "Answer Two", "Answer Three", "Answer Four"],["Answer One***", "Answer Two", "Answer Three", "Answer Four"]];
 
 let correctAnswers = {
     QuestionOneAnswer: "Answer Two",
@@ -8,23 +17,31 @@ let correctAnswers = {
 console.log(correctAnswers.QuestionOneAnswer)
 
 let quizImages = [
-    '<img src="assets/images/quiz-images/pexels-jf-valeriano-3609894.jpg" alt="Girl in a jacket">',
+    
     '<img src="assets/images/quiz-images/pexels-pixabay-162994.jpg" alt="">'
 ];
 
 let answerPosition = 0;
 let imagePosition = 0;
-let userAnswer = []
+let userAnswer = ["Answer Two"]
 
-let nextQuestion = document.getElementsByClassName("quiz_btns");
-nextQuestion.addEventListener('click', checkTheAnswer);
 
 // Questions 
 
 /**
  * checkTheAnswer function changes the color of the button/answer that has been clicked
  */
-function checkTheAnswer() {
-    console.log('checking answer')
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let answerBtns = document.getElementsByClassName("quiz_btns");
+    console.log(answerBtns);
+
+    for (let answer of answerBtns) {
+        answer.addEventListener("click", function() {
+            if (this.getAttribute('id').innerHTML === userAnswer){
+                answer.style.background = "green";
+            }
+            console.log(answer);
+        })
+    }
+})
 
