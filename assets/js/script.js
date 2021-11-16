b649bd6e2b885b96357d94a1ce55ea4ca8c9971a
rovan89 <devireland89@gmail.com>
2021-11-16 19:20:16 +0000

    Fix getQuestions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,47 @@ let questions = [
     {
         image: '<img src="assets/images/quiz-images/pexels-jf-valeriano-3609894.jpg" alt="Girl in a jacket">',
         answers: [
-            {text: 'Bond', corrent: true},
-            {text: '300', correct: false}
+            {text1: 'Bond', corrent: true},
+            {text2: '300', correct: false}
         ]
     },
 ]
 
 
-function createQuestions() {
+    
+function getQuestions() {
+    for(let i = 0; i < questions.length; i++){
+        console.log(questions[i])
+        for(const key of Object.values(questions[i])){
+            console.log(`${key} = ${questions[key][text1]}`)
+        }
+    }
+
+}
+getQuestions();
+
+function insertQuestions() {
     let answerDivContainer = document.getElementById('button_group_container');
     answerDivContainer.innerHTML = `
-        <button class="quiz_btns" id="answer_one" onclick="checkUserAnswer()">Answer One</button>
-        <button class="quiz_btns" id="answer_two" >Answer Two</button>
-        <button class="quiz_btns" id="answer_three">Answer Three</button>
-        <button class="quiz_btns" id="answer_four" >Answer Four</button>
+        <button class="quiz_btns" id="answer_one" onclick="checkUserAnswer()">${questions.values()}</button>
+        <button class="quiz_btns" id="answer_two" onclick="checkUserAnswer()">Answer Two</button>
+        <button class="quiz_btns" id="answer_three" onclick="checkUserAnswer()">Answer Three</button>
+        <button class="quiz_btns" id="answer_four"  onclick="checkUserAnswer()">Answer Four</button>
         `;
 }
-window.DOMContentLoaded = createQuestions();
+window.DOMContentLoaded = insertQuestions();
 
 
 
 
 function checkUserAnswer(){
     for(let i = 0; i < questions.length; i++){
-        let userAnswer = document.getElementsByClassName("quiz_btns");
-        
+        let userAnswer = document.getElementsByClassName("quiz_btns").innerHTML;
+        if(userAnswer == questions[i].answers){
+            
+            alert("It's working");
+        } else {
+            alert("else is working")
+        }
     }
-
 }
-
