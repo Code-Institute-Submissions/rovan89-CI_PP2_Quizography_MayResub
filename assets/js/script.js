8fc1639479df1563b248a9669ef91e18b7d7707a
rovan89 <devireland89@gmail.com>
2021-11-16 17:49:03 +0000

    Add create questions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,34 @@
 // This is an array of the questions and their images
-let quizAnswers = [
+let questions = [
     {
         image: '<img src="assets/images/quiz-images/pexels-jf-valeriano-3609894.jpg" alt="Girl in a jacket">',
         answers: [
             {text: 'Bond', corrent: true},
             {text: '300', correct: false}
         ]
-    }
-    
-    ["Answer One!!!", "Answer Two", "Answer Three", "Answer Four"],["Answer One***", "Answer Two", "Answer Three", "Answer Four"]];
+    },
+]
 
-let correctAnswers = {
-    QuestionOneAnswer: "Answer Two",
-    QuestionTwoAnswer: "Answer One***",
+
+function createQuestions() {
+    let answerDivContainer = document.getElementById('button_group_container');
+    answerDivContainer.innerHTML = `
+        <button class="quiz_btns" id="answer_one" onclick="checkUserAnswer()">Answer One</button>
+        <button class="quiz_btns" id="answer_two" >Answer Two</button>
+        <button class="quiz_btns" id="answer_three">Answer Three</button>
+        <button class="quiz_btns" id="answer_four" >Answer Four</button>
+        `;
 }
-console.log(correctAnswers.QuestionOneAnswer)
-
-let quizImages = [
-    
-    '<img src="assets/images/quiz-images/pexels-pixabay-162994.jpg" alt="">'
-];
-
-let answerPosition = 0;
-let imagePosition = 0;
-let userAnswer = ["Answer Two"]
+window.DOMContentLoaded = createQuestions();
 
 
-// Questions 
 
-/**
- * checkTheAnswer function changes the color of the button/answer that has been clicked
- */
-document.addEventListener("DOMContentLoaded", function() {
-    let answerBtns = document.getElementsByClassName("quiz_btns");
-    console.log(answerBtns);
 
-    for (let answer of answerBtns) {
-        answer.addEventListener("click", function() {
-            if (this.getAttribute('id').innerHTML === userAnswer){
-                answer.style.background = "green";
-            }
-            console.log(answer);
-        })
+function checkUserAnswer(){
+    for(let i = 0; i < questions.length; i++){
+        let userAnswer = document.getElementsByClassName("quiz_btns");
+        
     }
-})
+
+}
 
