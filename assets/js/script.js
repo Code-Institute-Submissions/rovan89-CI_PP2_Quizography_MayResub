d6dd038ebe120bd8a65276e4e9a24b76daeb588e
rovan89 <devireland89@gmail.com>
2021-11-19 21:04:49 +0000

    fix for each function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,23 +20,16 @@ let questions = [
     },
 ]
 
-let testAnswers = Object.values(questions[0]);
-console.log("Test:" + testAnswers)
 
 let testNum = -1;
 
-function getQuestions() {
-    for(let i = 0; i < questions.length; i++){
-        testNum = [i];
-        testAnswers = Object.values(questions[i]);
-        } 
-    }
-    console.log("test ANSWERS: " + testAnswers)
+let testAnswers = Object.values(questions);
+    console.log("test ANSWERS: " + testAnswers);
 
 let blah = document.getElementById('next_btn');
 blah.addEventListener("click", function() {
     testNum++;
-    getQuestions();
+    questions++;
     let answerDivContainer = document.getElementById('button_group_container');
     answerDivContainer.innerHTML = `
         <button class="quiz_btns" id="answer_one" onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
@@ -50,7 +43,6 @@ blah.addEventListener("click", function() {
     console.log("Please change");
 
 console.log(`Answer index position:${testNum}`);
-console.log("Answer text:" + testAnswers);
 console.log(blah);
 
 
