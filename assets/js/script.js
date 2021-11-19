571ad4ecbda3fac20dc455ba528520e1e8ed9b8c
rovan89 <devireland89@gmail.com>
2021-11-19 20:22:07 +0000

    break

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,19 +23,20 @@ let questions = [
 let testAnswers = Object.values(questions[0]);
 console.log("Test:" + testAnswers)
 
-let testNum = 0;
+let testNum = -1;
 
 function getQuestions() {
     for(let i = 0; i < questions.length; i++){
-       // testNum = [i];
+        testNum = [i];
         testAnswers = Object.values(questions[i]);
         } 
     }
-
+    console.log("test ANSWERS: " + testAnswers)
 
 let blah = document.getElementById('next_btn');
 blah.addEventListener("click", function() {
     testNum++;
+    getQuestions();
     let answerDivContainer = document.getElementById('button_group_container');
     answerDivContainer.innerHTML = `
         <button class="quiz_btns" id="answer_one" onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
@@ -43,18 +44,14 @@ blah.addEventListener("click", function() {
         <button class="quiz_btns" id="answer_three" onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
         <button class="quiz_btns" id="answer_four"  onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
         `;
-    insertQuestions();
     });
     
     console.log(testNum);
     console.log("Please change");
-    insertQuestions();
-    console.log(insertQuestions());
 
 console.log(`Answer index position:${testNum}`);
 console.log("Answer text:" + testAnswers);
 console.log(blah);
-console.log(getQuestions())
 
 
 
