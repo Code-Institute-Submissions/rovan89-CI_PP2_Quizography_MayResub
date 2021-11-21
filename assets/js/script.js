cb761bfea2206634b6d108c78df65bed5bc818a5
rovan89 <devireland89@gmail.com>
2021-11-21 10:03:25 +0000

    Fix question iteration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,31 @@
 // This is an array of the questions and their images
 let questions = [
     {
-        answerOne: "Bond",
-        answerTwo: "The Matrix",
-        answerThree: "Happy Gilmore",
-        answerFour: "You"
+        answer: ["Bond", "The Matrix", "Happy Gilmore", "You"],
+        correctAnswer: "Bond"
     },
     {
-        answerOne: "Thor",
-        answerTwo: "The Hulk",
-        answerThree: "Black Panther",
-        answerFour: "Captian America"
-    },
-    {
-        answerOne: "Hey",
-        answerTwo: "",
-        answerThree: "",
-        answerFour: ""
+        answer: ["Thor", "The Hulk", "Black Panther", "Captian America"],
+        correctAnswer: "The Hulk"
     },
 ]
 
 
-let testNum = -1;
-
-let testAnswers = Object.values(questions);
-    console.log("test ANSWERS: " + testAnswers);
-
-let blah = document.getElementById('next_btn');
-blah.addEventListener("click", function() {
-    testNum++;
-    questions++;
-    let answerDivContainer = document.getElementById('button_group_container');
-    answerDivContainer.innerHTML = `
-        <button class="quiz_btns" id="answer_one" onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
-        <button class="quiz_btns" id="answer_two" onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
-        <button class="quiz_btns" id="answer_three" onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
-        <button class="quiz_btns" id="answer_four"  onclick="checkUserAnswer()">${testAnswers[testNum]}</button>
-        `;
-    });
-    
-    console.log(testNum);
-    console.log("Please change");
-
-console.log(`Answer index position:${testNum}`);
-console.log(blah);
+function quizAnswers(){
+    let count = 0;
+    let questionOne = document.getElementById("answer_one");
+    let questionTwo = document.getElementById("answer_two");
+    let questionThree = document.getElementById("answer_three");
+    let questionFour = document.getElementById("answer_four");
+    for( let i = 0; i < questions.length; i++){
+        count = count++;
+        questionOne.innerHTML = questions[count].answer[0];
+        questionTwo.innerHTML = questions[count].answer[1];
+        questionThree.innerHTML = questions[count].answer[2];
+        questionFour.innerHTML = questions[count].answer[3];
+    }
+    console.log(count)
+}
 
 
 
