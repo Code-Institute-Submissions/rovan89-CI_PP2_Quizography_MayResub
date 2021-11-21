bc09366a02205f947048ef66ef09da319f4bed14
rovan89 <devireland89@gmail.com>
2021-11-21 20:23:50 +0000

    Add form to take user name input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // This is an array of the questions and their images
+console.log("Hi")
 let questions = [
     {
         answer: ["Bond", "The Matrix", "Happy Gilmore", "You"],
@@ -13,19 +14,22 @@ let questions = [
 
 function quizAnswers(){
     let count = 0;
+    console.log("This is count: " + count);
     let questionOne = document.getElementById("answer_one");
     let questionTwo = document.getElementById("answer_two");
     let questionThree = document.getElementById("answer_three");
     let questionFour = document.getElementById("answer_four");
+    count++;
     for( let i = 0; i < questions.length; i++){
-        count = count++;
         questionOne.innerHTML = questions[count].answer[0];
         questionTwo.innerHTML = questions[count].answer[1];
         questionThree.innerHTML = questions[count].answer[2];
         questionFour.innerHTML = questions[count].answer[3];
     }
-    console.log(count)
+    console.log("Count at end of function" + count);
 }
+console.log("Outside function: " + count);
+console.log("Why is console log not working?")
 
 
 
